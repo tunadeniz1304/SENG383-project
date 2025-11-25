0042695918492b6199a01b15f93b9fb051f99103
Tuna Deniz <136923631+tunadeniz1304@users.noreply.github.com>
2025-11-25 21:16:01 +0300

    Add files via upload

--- KidTask/src/model/Wish.java
+++ KidTask/src/model/Wish.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Wish {
-	
+    private String name;
+    private int cost;
+    private String owner;
+    private WishStatus status;
+    private int requiredLevel;
+
+    public Wish(String name, int cost, String owner, int requiredLevel) {
+        this.name = name;
+        this.cost = cost;
+        this.owner = owner;
+        this.status = WishStatus.PENDING;
+        this.requiredLevel = requiredLevel;
+    }
 }
