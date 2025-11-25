c0b9c8832ff6936ff644dce6f7ebe40ff8e1e085
Tuna Deniz <136923631+tunadeniz1304@users.noreply.github.com>
2025-11-25 20:49:24 +0300

    Add files via upload

--- KidTask/src/model/Task.java
+++ KidTask/src/model/Task.java
@@ -1,5 +1,25 @@
 package model;
+import java.time.LocalDate;
 
 public class Task {
-
+	private int id;
+	private String title;
+	private String description;
+    private int points;
+    private TaskStatus status;
+    private TaskType type;
+    private String assignedTo;
+    private LocalDate dueDate;
+    private int rating;
+    
+    public Task(int id, String title, int points, TaskType type, String assignedTo,int rating) {
+        this.id = id;
+        this.title = title;
+        this.points = points;
+        this.type = type;
+        this.assignedTo = assignedTo;
+        this.status = TaskStatus.PENDING;
+        this.dueDate = LocalDate.now();
+        this.rating = rating;
+    }
 }
